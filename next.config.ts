936da0f2b76afd0d3669d432fb4201173c855b99
alihamasdev <alihamasdev@gmail.com>
2025-09-08 14:07:53 +0500

    add tanstack-query, next-themes, nuqs provider, shadcn components and prisma schema + config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+	typedRoutes: true,
+	experimental: {
+		typedEnv: true,
+		viewTransition: true,
+		authInterrupts: true,
+		devtoolSegmentExplorer: true
+	}
 };
 
 export default nextConfig;
